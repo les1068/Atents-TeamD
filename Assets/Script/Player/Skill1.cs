b9dd20b5cf2b4a812aa751215103b28d7da02fa5
lich1114 <lich1114@gmail.com>
2023-03-09 14:44:16 +0900

    타겟 시스템 skill1 제작중
    이창화

---
+++ Assets/Script/Player/Skill1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill1 : Radar
+{
+    /// <summary>
+    /// 스킬 처리용 프로퍼티
+    /// </summary>
+    private float skillPower
+    {
+        
+        get => skillPower;
+        set
+        {
+            skillPower = str * value;
+                
+        }
+    }
+
+    
+    
+
+
+
+
+}
