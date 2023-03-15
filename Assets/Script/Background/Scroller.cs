9099969c359171b3a0d3d787ca99025d6f8af789
les1068 <81298309+les1068@users.noreply.github.com>
2023-03-15 14:21:46 +0900

    BackGroundScroll 변경
    민경빈

--- Assets/Script/Background/Scroller.cs
+++ Assets/Script/Background/Scroller.cs
@@ -4,24 +4,11 @@ using UnityEngine;
 
 public class Scroller : MonoBehaviour
 {
-    // 기본 원리
-    // 자식으로 붙어있는 BG_Slot이 계속 왼쪽으로 움직임
-    // 하나의 BG_Slot이 화면에 보이지 않을 때까지 왼쪽으로 이동하면 BG_Slot의 가장 오른쪽으로 이동 시킴
+    public float scrollingSpeed = 2.5f;  // 스크롤 이동 속도
 
-    /// <summary>
-    /// 이동 속도
-    /// </summary>
-    public float scrollingSpeed = 2.5f;
+    Transform[] bgSlots = null;  // 배경 이미지가 두개 붙어있는 슬롯의 집합
 
-    /// <summary>
-    /// 배경 이미지가 두개 붙어있는 슬롯의 집합
-    /// </summary>
-    Transform[] bgSlots = null;
-
-    /// <summary>
-    /// 이미지 한변의 길이
-    /// </summary>
-    float slot_Width = 13.6f;
+    float slot_Width = 13.6f;  // 이미지 한변의 길이
 
     protected virtual void Awake()
     {        
@@ -35,27 +22,17 @@ public class Scroller : MonoBehaviour
 
     private void Update()
     {
-        // 아래 foreach와 같은 코드. 하지만 foreach가 더 빠르다.
-        for (int i = 0; i < bgSlots.Length; i++)
+        for (int i = 0; i < bgSlots.Length; i++)  // 아래 foreach와 같은 코드. 하지만 foreach가 더 빠르다.
         {
             Transform slot = bgSlots[i];
             slot.Translate(Time.deltaTime * scrollingSpeed * -transform.right);
             if (slot.localPosition.x < 0)      // 슬롯이 부모 위치보다 왼쪽으로 갔을 때
             {
-                // 슬롯을 오른쪽 끝으로 이동 시키기
-                MoveRightEnd(i);
+                
+                MoveRightEnd(i);  // 슬롯을 오른쪽 끝으로 이동 시키기
             }
         }
-        //foreach(Transform slot in bgSlots)      // 모든 슬롯을 돌면서
-        //{
-        //    // 슬롯을 왼쪽으로 이동시키고
-        //    slot.Translate(Time.deltaTime * scrollingSpeed * -transform.right);     
-        //    if( slot.localPosition.x < 0 )      // 슬롯이 부모 위치보다 왼쪽으로 갔을 때
-        //    {
-        //        // 슬롯을 오른쪽 끝으로 이동 시키기
-        //        slot.Translate(slot_Width * bgSlots.Length * transform.right); 
-        //    }
-        //}
+       
     }
 
     protected virtual void MoveRightEnd(int index)
