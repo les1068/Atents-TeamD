bf34a59e12d8a7547b621defb168a27200cc8b78
Oh-in-ok <121930758+Oh-in-ok@users.noreply.github.com>
2023-04-28 01:14:16 +0900

    HP 슬라이더 UI 수정
    오인옥

--- Assets/Script/UI/Canvas_Player.cs
+++ Assets/Script/UI/Canvas_Player.cs
@@ -14,6 +14,7 @@ public class Canvas_Player : MonoBehaviour
     TextMeshProUGUI text_Skill3;
     TextMeshProUGUI text_Score;
     TextMeshProUGUI text_Exp;
+    Slider sliderHP;
 
     Player player;
     Skill1 skill1;
@@ -39,11 +40,13 @@ public class Canvas_Player : MonoBehaviour
         text_Skill2 = tran_Skill2.GetComponentInChildren<TextMeshProUGUI>();
         text_Skill3 = tran_Skill3.GetComponentInChildren<TextMeshProUGUI>();
 
-        Transform tran_Text = transform.GetChild(5);
-        Transform tran_Score = tran_Text.GetChild(0);
-        Transform tran_Exp = tran_Text.GetChild(1);
+        sliderHP = transform.GetChild(4).GetChild(1).GetComponent<Slider>();
 
+        Transform tran_Text = transform.GetChild(5);
+        
+        Transform tran_Score = tran_Text.GetChild(0);
         text_Score = tran_Score.GetComponent<TextMeshProUGUI>();
+        Transform tran_Exp = tran_Text.GetChild(1);
         text_Exp = tran_Exp.GetComponent<TextMeshProUGUI>();
     }
 
@@ -73,13 +76,18 @@ public class Canvas_Player : MonoBehaviour
         player = FindObjectOfType<Player>();
 
         player.onScoreChange += Refresh_Score;
-        player.onEXPChange += Refresh_Exp;        
+        player.onEXPChange += Refresh_Exp;
+
+        sliderHP.minValue = 0f;
+        sliderHP.maxValue = player.maxHp;
+        sliderHP.value = player.maxHp;
     }
 
     private void Update()
     {
         text_Score.text = $"{targetScore}";
         text_Exp.text = $"{targetExp}";
+        sliderHP.value = player.HP;
     }
 
     void Refresh_Score(int newScore)
